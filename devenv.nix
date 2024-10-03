3647b819d997649a0a047036ca68334feb0bfd0d
Dom Rodriguez <shymega@shymega.org.uk>
2024-10-03 12:20:05 +0100

    chore: Tweak, and enable settings in `devenv.nix`
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- devenv.nix
+++ devenv.nix
@@ -1,16 +1,26 @@
 { ... }:
 
 {
-  languages.rust = {
-    enable = true;
-    channel = "stable";
-    components = [ "rustc" "cargo" "clippy" "rustfmt" "rust-analyzer" ];
+  languages = {
+    rust = {
+      enable = true;
+      channel = "stable";
+      components = [ "rustc" "cargo" "clippy" "rustfmt" "rust-analyzer" ];
+    };
+    nix.enable = true;
+    shell.enable = true;
   };
 
   pre-commit.hooks = {
     rustfmt.enable = true;
     clippy.enable = true;
+    actionlint.enable = true;
+    commitizen.enable = true;
+    markdownlint.enable = true;
+    nixpkgs-fmt.enable = true;
   };
 
   devcontainer.enable = true;
+  dotenv.enable = true;
+  difftastic.enable = true;
 }
